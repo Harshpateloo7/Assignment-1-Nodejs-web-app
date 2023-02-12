fea350b7cbece43aa0a905de781cf44b8460e6ac
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-02-12 18:22:45 -0500

    Assignment 1 Node Js web aap

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Assignment-1-Nodejs-web-app
+# Image Citation Links
+1) https://usabilitygeek.com/psychology-web-design-influence-choice/
+2) https://www.friktoria.com/en/what-is-web-hosting.html
+3) https://wjmediagroup.com/what-is-social-media-marketing/
+4) https://www.techradar.com/how-to/photography-video-capture cameras77-photography-techniques-tips-and-tricks-for-taking-pictures-of-anything-1320768
+5) https://www.premiumbeat.com/blog/10-best-cameras-for-filmmaking-2021/
  
---
+++ index.js
@@ -0,0 +1,54 @@
+//Import Required Module
+const { response } = require("express");
+const express = require("express");
+const { request } = require("http");
+const path = require("path");
+const { MongoClient} = require("mongodb");
+
+//Mongo config stuff
+const dbUrl = "mongodb+srv://darshdeveloperdb:myFuqY1b6wzgUntN@cluster0.6exmnze.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(dbUrl);
+
+//Set up express app and port number
+const app = express();
+const port = process.env.PORT || 8888;
+
+//Set up template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+//Set up static file paths
+app.use(express.static(path.join(__dirname, "public")));
+
+//Page Routes
+app.get("/", async(request, response) => {
+ var services = await getServices();
+  response.render("index", { title: "Home", data: services});
+});
+app.get("/about", (request, response) => {
+  response.render("about", { title: "About"});
+});
+app.get("/contact",(request, response)=>{
+  response.render("contact",{ title:"Contact"});
+
+});
+
+//Setup server listning
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+//Mongo function
+/* function to connect to DB and return the darshdeveloperdb database. */
+async function connection(){
+  await client.connect();
+  db = client.db("darshdeveloperdb");
+  return db;
+}
+/*function to select all data from atlas */
+async function getServices(){
+    db = await connection();
+    var results = db.collection("Service").find({});
+    res = await results.toArray();
+    return res;
+}
